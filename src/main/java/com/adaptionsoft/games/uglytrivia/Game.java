0b493ade214fc5c249c51c904340af9d302f9c79
fghimpet <filip.ghimpeteanu@gmail.com>
2023-03-16 03:19:07 +0200

    Use parameterized Collections

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -5,15 +5,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Game {
-    ArrayList players = new ArrayList();
-    int[] places = new int[6];
-    int[] purses  = new int[6];
-    boolean[] inPenaltyBox  = new boolean[6];
-    
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
+	ArrayList<String> players = new ArrayList<>();
+	int[] places = new int[6];
+	int[] purses = new int[6];
+	boolean[] inPenaltyBox = new boolean[6];
+
+	LinkedList<String> popQuestions = new LinkedList<>();
+	LinkedList<String> scienceQuestions = new LinkedList<>();
+	LinkedList<String> sportsQuestions = new LinkedList<>();
+	LinkedList<String> rockQuestions = new LinkedList<>();
 
 	int currentPlayer = 0;
 	boolean isGettingOutOfPenaltyBox;
